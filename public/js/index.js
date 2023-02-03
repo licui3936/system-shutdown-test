6d29b78d5b0eead10f900f5a51fbcdf4dca4ec81
Li Cui <li@openfin.co>
2023-02-03 17:08:30 -0500

    add test for registerShutdownHandler

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,12 @@ async function initialise() {
     const ofVersion = document.querySelector('#of-version');
     ofVersion.innerText = await fin.System.getVersion();
 
+    const messageWindowBtn = document.querySelector('#message-window');
+    messageWindowBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+
+        fin.System.launchExternalProcess({alias: 'messageWin'});
+    })    
 
     // subscribing to the run-requested events will allow us to react to secondary launches, clicking on the icon once the Application is running for example.
     // for this app we will  launch a child window the first the user clicks on the desktop.
@@ -25,8 +31,8 @@ async function initialise() {
         e.preventDefault();
 
         const pltfm = fin.Platform.getCurrentSync();
-        let myWin = await pltfm.createWindow({
-            url: "http://127.0.0.1:5555/main.html",
+        await pltfm.createWindow({
+            url: "http://localhost:5555/main.html",
             name: "Dynamic Generated Platform Window",
             contextMenu: true,
             contextMenuSettings: { enable: true, devtools: true, reload: true },
@@ -41,7 +47,7 @@ async function initialise() {
                                 componentName: 'view',
                                 componentState: {
                                     name: 'child_view',
-                                    url: 'http://127.0.0.1:5555/child.html'
+                                    url: 'http://localhost:5555/child.html'
                                 }
                             }
                         ]
@@ -52,32 +58,50 @@ async function initialise() {
 
     });
 
-    let childWinIdentity = null;
+    const platformWindowHandlerBtn = document.querySelector('#platform-window-handler');
+    platformWindowHandlerBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
 
-    let count = 0;
+        const pltfm = fin.Platform.getCurrentSync();
+        await pltfm.createWindow({
+            url: "http://localhost:5555/main.html",
+            name: "Dynamic Generated Platform Window",
+            contextMenu: true,
+            contextMenuSettings: { enable: true, devtools: true, reload: true },
+            processAffinity: "platformProcess",
+            layout: {
+                content: [
+                    {
+                        type: 'stack',
+                        content: [
+                            {
+                                type: 'component',
+                                componentName: 'view',
+                                componentState: {
+                                    name: 'child_view',
+                                    url: 'http://localhost:5555/child.html'
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        });
+        fin.System.registerShutdownHandler(proceed => {
+            console.log('do cleanup in platform window');
+            proceed();
+        })
+    });
 
     const childWindowBtn = document.querySelector('#child-window');
     childWindowBtn.addEventListener('click', async (e) => {
-
-        count += 1;
-        childWinIdentity = await fin.Window.create(
+        let time = Date.now();
+        await fin.Window.create(
             {
-                name: 'child' + count,
-                // maxHeight: 144,
+                name: 'child_' + time,
                 autoShow: true,
-                //alwaysOnTop: true,
-                //defaultWidth: 94,
-                //minWidth: 94,
-                //defaultCentered: false,
                 frame: true,
-                //resizable: false,
-                //showTaskbarIcon: false,
-                //minimizable: false,
-                //smallWindow: true,
-                //maximizable: false,
-                //saveWindowState: false,
-                //includeInSnapshots: false,
-                url: 'http://127.0.0.1:5555/child.html', // The URL of the View
+                url: 'http://localhost:5555/child.html', // The URL of the View
                 accelerator: {
                     zoom: true
                 },
@@ -91,10 +115,34 @@ async function initialise() {
                     ]
                 }
             });
+    });
 
-        childWinIdentity.disableUserMovement();
-        childWinIdentity.addListener('disabled-movement-bounds-changing', d => console.log(d));
-
+    const childWindowHandlerBtn = document.querySelector('#child-window-handler');
+    childWindowHandlerBtn.addEventListener('click', async (e) => {
+        let time = Date.now();
+        await fin.Window.create(
+            {
+                name: 'child_' + time,
+                autoShow: true,
+                frame: true,
+                url: 'http://localhost:5555/child.html', // The URL of the View
+                accelerator: {
+                    zoom: true
+                },
+                processAffinity: "childProcess",
+                permissions: {
+                    System: {
+                        launchExternalProcess: true
+                    },
+                    webAPIs: [
+                        "openExternal"
+                    ]
+                }
+            });
+        fin.System.registerShutdownHandler(proceed => {
+            console.log('do cleanup in child window');
+            proceed();
+        })
     });
 
     const newViewBtn = document.querySelector('#new-view');
@@ -102,14 +150,18 @@ async function initialise() {
         e.preventDefault();
 
         const pltfm = fin.Platform.getCurrentSync();
-        pltfm.createView({ url: "http://localhost:5555/index.html" }, win.identity, fin.me.identity)
-    })
+        await pltfm.createView({ url: "http://google.com" }, win.identity, fin.me.identity);
+    });
 
-    const nonOFwindow = document.querySelector('#nonOFwindow');
-    nonOFwindow.addEventListener('click', async (e) => {
+    const newViewHandlerBtn = document.querySelector('#new-view-handler');
+    newViewHandlerBtn.addEventListener('click', async (e) => {
         e.preventDefault();
 
-        window.open("https://www.wikipedia.org", "nonOFwindow")
-    })
-
+        const pltfm = fin.Platform.getCurrentSync();
+        await pltfm.createView({ url: "http://google.com" }, win.identity, fin.me.identity);
+        fin.System.registerShutdownHandler(proceed => {
+            console.log('do cleanup in view');
+            proceed();
+        })
+    });
 }
